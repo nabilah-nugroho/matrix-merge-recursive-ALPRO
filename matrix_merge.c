1a23bbc72243f5b7f16ba93765dd922c17ee8d6f
nabilah nugroho <boredluckykid@gmail.com>
2026-04-07 11:20:21 +0700

    added matrix input

---
+++ matrix_merge.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define tungguDulu 10
+int n; //kolom
+int b = 2; //baris
+
+int matriks1[2][tungguDulu];
+int matriks2[2][tungguDulu];
+
+void inputUser(){
+    printf("masukkan kolom yang diinginkan: "); scanf("%d", &n);
+    printf("\n\n");
+
+    printf("input matriks 1\n");
+    for (int i = 0; i < b; i++){
+        for (int j = 0; j < n; j++){
+            printf("matriks1[%d][%d]: ", i+1, j+1);
+            scanf("%d", &matriks1[i][j]);
+        }
+    }
+    printf("\n\n");
+    printf("input matriks 2\n");
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < b; j++){
+            printf("matriks2[%d][%d]: ", i+1, j+1);
+            scanf("%d", &matriks2[i][j]);
+        }
+    }
+}
+
+int main() {
+    inputUser();
+    return 0; 
+}
