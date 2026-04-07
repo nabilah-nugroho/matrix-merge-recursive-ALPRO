17342cbc4415279607c307d9126297c1befb3f86
nabilah nugroho <boredluckykid@gmail.com>
2026-04-07 12:35:45 +0700

    added merge with for loop n fixed static loop

--- matrix_merge.c
+++ matrix_merge.c
@@ -7,6 +7,7 @@ int b = 2; //baris
 int matriks1[2][tungguDulu];
 int matriks2[2][tungguDulu];
 
+
 void inputUser(){
     printf("masukkan kolom yang diinginkan: "); scanf("%d", &n);
     printf("\n\n");
@@ -20,15 +21,36 @@ void inputUser(){
     }
     printf("\n\n");
     printf("input matriks 2\n");
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < b; j++){
+    for (int i = 0; i < b; i++){
+        for (int j = 0; j < n; j++){
             printf("matriks2[%d][%d]: ", i+1, j+1);
             scanf("%d", &matriks2[i][j]);
         }
     }
 }
 
+void GSS() {
+
+for (int i = 0; i < b; i++){
+    if (i % 2 == 0){
+        for (int j = 0; j < n; j++){
+            printf("%d ", matriks1[i/2][j]);
+            }
+    printf("\n");
+        }
+
+    else {
+        for (int j = 0; j < n; j++){
+            printf("%d ", matriks2[i/2][j]);
+            }
+    printf("\n");
+        }
+    }
+
+}
+
 int main() {
     inputUser();
+    GSS();
     return 0; 
 }
